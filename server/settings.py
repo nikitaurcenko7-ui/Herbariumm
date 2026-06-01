5b12f29fec7e277cf5c08d0b7a14eca2f48c8182
nikitaurcenko7-ui <nikitaurcenko7@gmail.com>
2026-06-02 01:03:59 +1000

    Support persistent database URL

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,12 @@
+import os
 from pathlib import Path
+
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    import dj_database_url
+except ImportError:
+    dj_database_url = None
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,12 +56,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    if dj_database_url is None:
+        raise ImproperlyConfigured('DATABASE_URL is set, but dj-database-url is not installed.')
+
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Asia/Vladivostok'
